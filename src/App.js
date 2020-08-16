b85f327b61a6bf4c9c8715327eea2528316e8660
Sabina Barbhuiya <sabina.barbhuiya@yahoo.com>
2020-08-16 22:56:03 +0100

    Fixed warnings

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import PlanetList from './PlanetList';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
   render(){
     return(
       <div className="App">
--- src/Planet.js
+++ src/Planet.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import './Planet.css';
-import PropTypes from 'prop-types'
 
-
-function Planet(props){
-    return(
-    <div className="planet" >
-        <div className="overlay"></div>
-        <img className="planet-img" src={props.data.image} alt="planet" />
-        <div className="planet-content">
-            <div className="planet-name">{props.data.name}</div>
-            <p className="planet-age">{props.planetAge(props.data.earthYears, props.age)} Year(s)</p>
-            <p className="planet-weight">{props.planetWeight(props.data.earthWeight, props.weight)} Kg</p>
+function Planet(props) {
+    return (
+        <div className="planet" >
+            <div className="overlay"></div>
+            <img className="planet-img" src={props.data.image} alt="planet" />
+            <div className="planet-content">
+                <div className="planet-name">{props.data.name}</div>
+                <p className="planet-age">{props.planetAge(props.data.earthYears, props.age)} Year(s)</p>
+                <p className="planet-weight">{props.planetWeight(props.data.earthWeight, props.weight)} Kg</p>
+            </div>
         </div>
-    </div>
     )
 }
 
 
 Planet.defaultProps = {
-    planetAge: (earthYears, ageInYears) => Number((ageInYears/earthYears).toFixed(0)),
-    planetWeight: (earthWeight, weightInKg) => Number((earthWeight*weightInKg).toFixed(0))
+    planetAge: (earthYears, ageInYears) => Number((ageInYears / earthYears).toFixed(0)),
+    planetWeight: (earthWeight, weightInKg) => Number((earthWeight * weightInKg).toFixed(0))
 }
-
-// Planet.propTypes = {
-//     secondsInEarthYear : PropTypes.number
-// }
 
 export default Planet;
